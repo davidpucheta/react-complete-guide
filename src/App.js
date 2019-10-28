623184c3b14728edad8302206d338094b1b10ed7
David Pucheta <davidpucheta@gmail.com>
2019-10-28 12:29:32 -0600

    Add person component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
   return (
     <div className="App">
       <h1>Hi, I'm a React app</h1>
       <p>This is really working!!!</p>
+      <Person />
     </div>
   );
 }
