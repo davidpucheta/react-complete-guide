3e5a8c46b9e90059431b2a0bb0cc3cc782c7cb0c
David Pucheta <davidpucheta@gmail.com>
2019-10-28 12:48:58 -0600

    Understanding the  Children Property

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <h1>Hi, I'm a React app</h1>
       <p>This is really working!!!</p>
       <Person name="Max" age="28"/>
-      <Person name="Manu" age="29"/>
+      <Person name="Manu" age="29"> My Hobbies: Racing </Person>
       <Person name="Stephanie" age="26"/>
     </div>
   );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,10 @@ import React from 'react';
 const person = (props) => {
 
     return (
-        <p>I'm {props.name} and I am {props.age} years old!</p>
+        <div>
+            <p>I'm {props.name} and I am {props.age} years old!</p>
+            <p>{props.children}</p>
+        </div>
     );
 };
 
