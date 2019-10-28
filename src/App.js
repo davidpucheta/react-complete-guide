907ee9622f7d6a761737496b110f5305f35423a5
David Pucheta <davidpucheta@gmail.com>
2019-10-28 16:38:14 -0600

    Passing method references between components

--- src/App.js
+++ src/App.js
@@ -13,11 +13,11 @@ class App extends Component {
     otherState: 'some other value'
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('Was clicked!');
     this.setState({
       persons: [
-        { name: "Maximilian", age: 28 },
+        { name: newName, age: 28 },
         { name: "Manu", age: 29 },
         { name: "Stephanie", age: 27 }
       ] 
@@ -30,11 +30,22 @@ class App extends Component {
         <h1>Hi, I'm a React app</h1>
         <p>This is really working!!!</p>
 
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button onClick={ () => this.switchNameHandler('Maxxxx!!!') }>Switch Name</button>
 
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> My Hobbies: Racing </Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <Person 
+        name={this.state.persons[0].name} 
+        age={this.state.persons[0].age} 
+        click={this.switchNameHandler.bind(this, 'New Name 01')}/>
+        
+        <Person 
+        name={this.state.persons[1].name} 
+        age={this.state.persons[1].age} 
+        click={this.switchNameHandler.bind(this, 'New Name 02')} >
+        My Hobbies: Racing </Person>
+        
+        <Person 
+        name={this.state.persons[2].name} 
+        age={this.state.persons[2].age}/>
       </div>
     );
   }
