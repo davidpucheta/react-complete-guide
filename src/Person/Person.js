3f0179c1115ef843bdb26c7f4bdf07dfa7bdd15d
David Pucheta <davidpucheta@gmail.com>
2019-10-28 12:39:07 -0600

    Output dynamic content

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const person = () => {
     return (
-        <p>I'm a Person!</p>
+        <p>I'm a Person and I am {Math.floor(Math.random() * 30)} years old!</p>
     );
 };
 
