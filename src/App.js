dc0102d497aaeb8fdc9046a66a08e8ce546421eb
David Pucheta <davidpucheta@gmail.com>
2019-10-28 18:37:16 -0600

    Flexible lists

--- src/App.js
+++ src/App.js
@@ -14,13 +14,22 @@ class App extends Component {
     showPersons: false
   }
 
-  nameChangedHandler = (event) => {
+  nameChangedHandler = (event, id) => {
+
+    const personIndex = this.state.persons.findIndex(
+      (p) => {
+        return p.id === id;
+      }
+    );
+
+    const person = { ...this.state.persons[personIndex] };
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
     this.setState({
-      persons: [
-        { name: 'Max', age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: "Stephanie", age: 26 }
-      ] 
+      persons: persons
     });
   };
 
@@ -55,7 +64,8 @@ class App extends Component {
               click={ () => this.deletePersonHandler(index) }
               name={person.name}
               age={person.age} 
-              key={person.id} />
+              key={person.id} 
+              changed={ (event) => this.nameChangedHandler(event, person.id) }/>
           })
         }
       </div>);
