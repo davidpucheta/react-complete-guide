27e76520ce69f89a3139b08fd7cce94ad7419077
David Pucheta <davidpucheta@gmail.com>
2019-10-29 09:58:31 -0600

    Using radium for media queries

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import Radium from 'radium';
+import Radium, {StyleRoot} from 'radium';
 
 class App extends Component {
  
@@ -93,6 +93,7 @@ class App extends Component {
     }
 
     return (
+      <StyleRoot>
       <div className="App">
         <h1>Hi, I'm a React app</h1>
         <p className={classes.join(' ')}>This is really working!!!</p>
@@ -106,6 +107,7 @@ class App extends Component {
         {persons}
 
       </div>
+      </StyleRoot>
     );
   }
 }
