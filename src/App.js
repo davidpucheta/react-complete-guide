013002a6cc7cbec211ee0e727f32c352dbf00f13
David Pucheta <davidpucheta@gmail.com>
2019-10-28 18:00:45 -0600

    Handling Dynamic Content the JS way

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ class App extends Component {
       { name: "Manu", age: 29 },
       { name: "Stephanie", age: 26 }
     ] ,
-    otherState: 'some other value'
+    otherState: 'some other value',
+    showPersons: false
   }
 
   switchNameHandler = (newName) => {
@@ -34,6 +35,11 @@ class App extends Component {
     });
   };
 
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  };
+
   render() {
 
     const style = {
@@ -44,17 +50,10 @@ class App extends Component {
       cursor: 'pointer'
     }
 
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React app</h1>
-        <p>This is really working!!!</p>
+    let persons = null;
 
-        <button
-        style={style} 
-        onClick={ () => this.switchNameHandler('Maxxxx!!!') }>
-          Switch Name
-        </button>
-
+    if (this.state.showPersons) {
+      persons = (<div >
         <Person 
         name={this.state.persons[0].name} 
         age={this.state.persons[0].age} 
@@ -71,6 +70,22 @@ class App extends Component {
         <Person 
         name={this.state.persons[2].name} 
         age={this.state.persons[2].age}/>
+      </div>);
+    }
+
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React app</h1>
+        <p>This is really working!!!</p>
+
+        <button
+        style={style} 
+        onClick={ this.togglePersonsHandler }>
+          Toggle Persons
+        </button>
+
+        {persons}
+
       </div>
     );
   }
