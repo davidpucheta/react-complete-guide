cc4fb54244d803260d5e5d8f0d81b78a98bc6136
David Pucheta <davidpucheta@gmail.com>
2019-10-28 18:09:52 -0600

    Outputting lists

--- src/App.js
+++ src/App.js
@@ -54,22 +54,13 @@ class App extends Component {
 
     if (this.state.showPersons) {
       persons = (<div >
-        <Person 
-        name={this.state.persons[0].name} 
-        age={this.state.persons[0].age} 
-        click={this.switchNameHandler.bind(this, 'New Name 01')}/>
-        
-        <Person 
-        name={this.state.persons[1].name} 
-        age={this.state.persons[1].age} 
-        click={this.switchNameHandler.bind(this, 'New Name 02')} 
-        changed={this.nameChangedHandler}
-        >
-        My Hobbies: Racing </Person>
-        
-        <Person 
-        name={this.state.persons[2].name} 
-        age={this.state.persons[2].age}/>
+        {
+          this.state.persons.map( (person) => {
+            return <Person 
+              name={person.name}
+              age={person.age} />
+          })
+        }
       </div>);
     }
 
